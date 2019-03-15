6edd3f5ba7ec1f6bd10771c50d1c7a53a54d5a7e
Ashis Saha <ashissaha@Ashiss-MBP.fios-router.home>
2019-03-15 19:47:24 -0400

    datautil package

---
+++ datautil/R/tmm_normalize.R
@@ -0,0 +1,21 @@
+#' TMM-normalization of RNA-seq data
+#' 
+#' This function normalizes a gene expression matrix using TMM.
+#' @param expr_mat matrix. Data matrix where rows and columns represent genes and samples, respectively.
+#' @return Returns a transformed matrix.
+#' @examples 
+#' y <- matrix( rpois(1000, lambda=5), nrow=200 )
+#' tmm_y = tmm_normalize(y)
+#' @export
+#' @importFrom Rdpack reprompt
+#' @references 
+#' \insertRef{Robinson2010}{datautil}
+tmm_normalize <- function(expr_mat){
+  require('edgeR')
+  
+  # expr_mat: gene x sample matrix
+  stopifnot(class(expr_mat) == 'matrix')
+  tmm_factors = calcNormFactors(expr_mat, method = "TMM")
+  expr_mat = t(t(expr_mat)/tmm_factors)
+  return(expr_mat)
+}
---
+++ datautil/R/to_inv_normal.R
@@ -0,0 +1,18 @@
+#' Transformation data to inverse normal distribution
+#' 
+#' This function transforms values in each row of a matrix to a rank based inverse normal transformation.
+#' @param expr_mat matrix. Data matrix.
+#' @return Returns a transformed matrix.
+#' @examples 
+#' x = matrix(1:100, nrow=5, byrow=T, dimnames = list(paste0('gene', 1:5), paste0('sample', 1:20)))
+#' inv_norm_x = to_inv_normal(x)
+#' @export
+to_inv_normal <- function(expr_mat){
+  stopifnot(class(expr_mat) == 'matrix')
+  # expr_mat : gene x sample matrix
+  transformed_data <- apply(expr_mat, 1, function(x){
+    qnorm((rank(x,na.last="keep")-0.5)/sum(!is.na(x)))
+  })
+  transformed_data = t(transformed_data)
+  return(transformed_data)
+}
