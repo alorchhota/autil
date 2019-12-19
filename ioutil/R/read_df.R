b2e02efb61af63a20b742e6af739daa555dc325a
Ashis Saha <ashissaha@Ashiss-MBP.fios-router.home>
2019-12-19 08:00:02 -0500

    read_df() reads big integers as numeric, not as integer64.

--- ioutil/R/read_df.R
+++ ioutil/R/read_df.R
@@ -31,7 +31,8 @@ read_df <- function(fn, sep = '\t',  header = T, quote = "", row.names=T, string
                   quote = quote,    # not available in old package
                   stringsAsFactors = stringsAsFactors, 
                   check.names = check.names, 
-                  data.table = FALSE)
+                  data.table = FALSE, 
+                  integer64 = "numeric")
   if (row.names == TRUE){
     rownames(data_df) = data_df[,1]
     data_df = data_df[,-1, drop=F]
